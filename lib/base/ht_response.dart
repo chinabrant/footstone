a0506cc9315339c60e921852f338f03a9868b75c
brant <sjwu1234@gmail.com>
2020-01-09 20:28:20 +0800

    HTResponse添加status,msg,data字段

--- lib/base/ht_response.dart
+++ lib/base/ht_response.dart
@@ -16,4 +16,29 @@ class HTResponse<K> {
   final dynamic originalData;
 
   HTResponse({this.object, this.isSuccess, this.statusCode, this.errorMsg, this.originalData});
+
+  /// 接口正常返回后的status
+  int get status {
+    if (originalData != null && originalData['status'] is int) {
+      return originalData['status'];
+    }
+
+    return -1;
+  }
+
+  dynamic get data {
+    if (originalData == null) {
+      return null;
+    }
+    
+    return originalData['data'];
+  }
+
+  String get msg {
+    if (originalData == null) {
+      return null;
+    }
+
+    return originalData['msg'];
+  }
 }
